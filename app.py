37984e0cae10d26343bbe7a64c6360cf9068c9b4
MeiXinQing <meixinqing147@gmail.com>
2023-09-11 22:17:14 +0800

    完成了机器人回复对话

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__, template_folder="templates",static_folder="static")
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/reply',methods=['POST'])
+def reply():
+    reply="你好！"
+    return {'reply': reply}
 
 if __name__ == '__main__':
     app.run()
